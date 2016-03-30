801315a29920c5dbae953c9909a48b34453c367e
Andi Devois <devoisaurus@Andi-Devoiss-MacBook-Pro.local>
2016-03-30 17:02:27 -0500

    changed output code

--- quiz.js
+++ quiz.js
@@ -3,18 +3,28 @@
 	console.log("inCars1", inCars);
 
 var CarLot = (function populatePage (oldCarLot){
+
 	oldCarLot.displayCars = function(inventory){
+		var inCars = document.getElementsByClassName("container");
 		var seeTheCars="";
 		console.log("seeTheCars1", seeTheCars);
-  // Loop over the inventory and populate the page
-  inventory.forEach(function (currentCar) {
-  	var carList = inventory[currentCar];
-  		seeTheCars += "<div id='make" +currentCar.make+"'>Make:";
-  		seeTheCars += `<h2>${currentCar.make}</h2>`;
+		for (var i = 0; i < inventory.length; i++) {
+			console.log("i", i);
+			var currentCar = inventory[i];
+			console.log("inventory", currentCar);
+			seeTheCars+= `<div id="output" class="row"><div class="col-xs-4">
+			<p>${currentCar.year} ${currentCar.make} ${currentCar.model}</p>
+			<p>Price: ${currentCar.price}</p> Color:  ${currentCar.color} 
+			<p>${currentCar.purchased}</p>
+			<p>Description: ${currentCar.description}</p>
+			</div></div>`
+			console.log("seeTheCars", seeTheCars);
 
-  		inCars.innerHTML += seeTheCars;
+			inCars.innerHTML += seeTheCars;
+		};
 
-  })
+  // Loop over the inventory and populate the page 
+
 }
 	return oldCarLot;
 }(CarLot));
@@ -27,7 +37,8 @@ var CarLot = (function populatePage (oldCarLot){
   // Now that the DOM is loaded, establish all the event listeners needed
 //   CarLot.activateEvents();
 // }
-
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-// CarLot.loadInventory();
+
+
+CarLot.loadInventory(populatePage);
