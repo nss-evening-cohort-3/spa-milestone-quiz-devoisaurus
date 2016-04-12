e4bea0cbbb452c0d5bcc59e6f5f18ed1450dfc92
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-04-11 20:28:57 -0500

    border click event

--- quiz.js
+++ quiz.js
@@ -20,36 +20,39 @@
         		oldCarLot.activateEvents = function (carCards) {
 			for (var i = 0; i < carCards.length; i++) {
 				carCards[i].addEventListener("click", function(e){
-					console.log("howdy!");
 					let clickTarget = e.target.closest("div");
-					console.log("clickTarget", clickTarget);
                     selectCopy(e);  
+
 				})
 			}
 
             var copyEntry = document.getElementById("copyChange");
-            console.log("copyEntry", copyEntry);
             var newCopy = document.getElementsByClassName("oldcopy");
-            console.log("newCopy", newCopy);
-            var borderUp = document.getElementsByClassName("carCard");
-            console.log("borderUp", borderUp);
+            var originalCard = document.getElementsByClassName("carCard");
             var replaceCopy;
+
 
             function selectCopy (event) {
                 replaceCopy = event.target.closest(".carCard").childNodes[6].nextElementSibling;
                 copyEntry.value="";
                 copyEntry.focus();
-                console.log("replaceCopy", replaceCopy);
-                console.log("copyEntry", copyEntry);
                 var myCopy = copyEntry.addEventListener("keyup", changeCopy);
-                console.log("myCopy", myCopy);
-                console.log("copyEntry", copyEntry);
-                console.log("changeCopy", changeCopy);
+
             };
 
             function changeCopy(event) {
                 replaceCopy.innerHTML = copyEntry.value;
             }
+
+            function changeCard (event) {
+                event.target.closest(".carCard").style.borderWidth = "thick";
+                event.target.closest(".carCard").style.background = "pink";
+            }
+
+            for (var i = 0; i < originalCard.length; i++) {
+                originalCard[i].addEventListener("click", changeCard);
+            };
+
 		};
 
   // Loop over the inventory and populate the page 
